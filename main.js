1dd85148c2cb4aff85750f10274fd3f057da08de
Steven Renault <steven@MacBook-Air-de-Steven.local>
2019-11-23 22:40:30 +0100

    fixe listen + maj get and post +add put

--- main.js
+++ main.js
@@ -1,29 +1,50 @@
+
 const express = require('./my-express')
 const app = express()
+const url = require('url');
+const args = process.argv.slice(2)
 
-// app.get('/', function (req, res,name) {
-//   res.write(` <h1 style = 'color : green' >Hello ${name || 'World'} </h1>`);
-// })
+if (args.length !== 1) {
+  console.log("usage: node server.js <PORT_Name>");
+}
+else {
+  const port = parseInt(args[0])
 
-app.get('/', function (req, res, name) {
-  res.write("<h1 style = 'color : green' >Hello World </h1>");
-})
+  app.get('/', function (req, res) {
+    const request_url = req.url;
+    const { query } = url.parse(request_url, true)
+    const { name } = query
+    res.write(` <h1 style = 'color : green' >Hello ${name || 'World'} </h1>`);
+  })
+  
+  app.post('/students', function (req, res) {
+    res.write("POST request")
 
-app.post('/students', function (req, res) {
-  res.write("<h1> Utilisateur ajouté </h1>")
-})
+  })
 
-// app.put('/', function (req, res) {
-//   res.send('PUT request to homepage')
-// })
+  app.put('/students/2', function (req, res) {
+    res.write('PUT request')
+  })
 
-// app.delete('/', function (req, res) {
-//   res.send('DELETE request to homepage')
-// })
 
-// app.all('/secret', function (req, res, next) {
-//   console.log('Accessing the secret section ...')
-//   next() // pass control to the next handler
-// })
+    // // app.get('/home', function (req, res) {
+  // //   res.write('Hello Worlddssds!')
+  // // })
 
-app.listen(4242)
+  // app.put('/students/sff', function (req, res) {
+  //   res.write('PUT request')
+  // }) 
+
+  // app.delete('/', function (req, res) {
+  //   res.send('DELETE request to homepage')
+  // })
+
+  // app.all('/secret', function (req, res, next) {
+  //   console.log('Accessing the secret section ...')
+  //   next() // pass control to the next handler
+  // })
+
+  app.listen(port, () => {
+    console.log("app listening on port "+port)
+  })
+}
--- my-express.js
+++ my-express.js
@@ -1,8 +1,10 @@
 
 const http = require('http')
-const url = require('url');
 const fs = require('fs');
 const LOCAL_DATABASE = 'students.json'
+const url = require('url');
+
+
 
 
 class myExpress {
@@ -33,22 +35,22 @@ class myExpress {
 
     get(path, callback) {
         this.app.on('request', (req, res) => {
-
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+            
             let pathname = this.getPathname(req)
-            let query = this.getQuery(req)
-
-            // if (query != null) {
-            //     name = query.name
-            // }
             if (path == pathname && req.method == 'GET') {
                 callback(req, res)
                 res.end()
+
+            } else if (path !== pathname && req.method == 'GET') {
+                res.write('Cannot GET '+pathname)
+                res.end()
             }
+           
         })
 
     }
-// verifier quand on supprime un user et qu'on ajoute un nouveau user l'id soit unique
+    // verifier quand on supprime un user et qu'on ajoute un nouveau user l'id soit unique
+    // verfier si  le body n'est pas vide
     post(path, callback, ) {
         this.app.on('request', (req, res) => {
 
@@ -69,7 +71,7 @@ class myExpress {
                         user.id = 1
                         data = [user]
                     } else {
-                        const json = require(`./${LOCAL_DATABASE}`) 
+                        const json = require(`./${LOCAL_DATABASE}`)
                         user.id = json.length + 1
                         json.push(user)
                         data = json
@@ -81,12 +83,85 @@ class myExpress {
                     res.end()
 
                 })
-             
+
+            } else if (path !== pathname && req.method == 'POST') {
+                res.write('Cannot POST '+pathname)
+                res.end()
             }
         })
     }
 
-    put() {
+    put(path, callback) {
+
+        this.app.on('request', (req, res) => {
+
+            let pathname = this.getPathname(req)
+            let splitPathname = pathname.split('/')
+            let putId = splitPathname[splitPathname.length - 1]
+            let matchPut = []
+            let json = ""
+            let data = ''
+
+            
+            if (path == pathname && req.method == 'PUT') {
+
+                req.on('data', chunk => {
+                    data += chunk.toString()
+                })
+
+                req.on('end', () => {
+
+                    let putData = JSON.parse(data)
+                    const { name, school } = putData
+                    let isExist = false
+                    matchPut = putId.match(/(^[0-9]+$)/g) // ON VERIFIE SI ON MATCH AVEC UN NOMBRE
+
+
+                    if (matchPut != null) {// ON VERIFIE SI IL Y UN NOMBRE
+                        putId = parseInt(matchPut[0])
+                        if (fs.existsSync(LOCAL_DATABASE)) {
+                            json = require(`./${LOCAL_DATABASE}`) //on lit et parse en string
+                            //ON VERIFIE SI L'ID EXISTE
+                            for (const k in json) {
+                                if (json[k].id == putId) {
+                                    isExist = true
+                                }
+                            }
+                            // SI IL EXISTE ON CHERCHE LA LIGNE A MODIFIER
+                            if (isExist == true) {
+                                for (const key in json) {
+                                    if (json[key].id == putId) {
+                                        if (name != undefined) {
+                                            json[key].name = name
+                                        }
+                                        if (school != undefined) {
+                                            json[key].school = school
+                                        }
+                                        fs.writeFileSync(LOCAL_DATABASE, JSON.stringify(json, null, 4))
+                                        console.log("La modification a bien été aplliqué")
+                                    }
+                                }
+                            } else {
+                                console.log(` L'id ${putId} n'existe pas dans le fichier ${LOCAL_DATABASE}`)
+                            }
+
+                        } else {
+                            console.log(` Le fichier ${LOCAL_DATABASE} ne peut pas etre modifié si il n'existe pas`)
+                        }
+                    } else {
+                        console.log("L'id renseigné n'est pas un nombre ")
+                        
+                    }
+                    callback(req, res)
+                    res.end()
+                })
+
+            }
+            else if (path !== pathname && req.method == 'PUT'){
+                res.write('Cannot PUT '+pathname)
+                res.end()
+            }
+        })
 
     }
 
@@ -97,10 +172,10 @@ class myExpress {
     all() {
 
     }
-    listen(port) {
-    // Recuperer le port en ligne de commande
+    listen(port,callback) {
         if (typeof port == 'number') {
             this.app.listen(port)
+            callback()
         } else {
             console.log("le port n'est pas un nombre")
         }
