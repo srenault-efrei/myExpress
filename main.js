7a94031a3f2d752b5c6ea6195f977f290630934c
Steven Renault <steven@MacBook-Air-de-Steven.local>
2019-11-23 02:05:09 +0100

    create GET + POST

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const express = require('./my-express')
-const app =  express()
+const app = express()
 
+// app.get('/', function (req, res,name) {
+//   res.write(` <h1 style = 'color : green' >Hello ${name || 'World'} </h1>`);
+// })
 
+app.get('/', function (req, res, name) {
+  res.write("<h1 style = 'color : green' >Hello World </h1>");
+})
 
+app.post('/students', function (req, res) {
+  res.write("<h1> Utilisateur ajouté </h1>")
+})
 
-app.get('/home', function (req, res) {
-  res.write('hello')
-  })
+// app.put('/', function (req, res) {
+//   res.send('PUT request to homepage')
+// })
 
- app.listen(4242)
+// app.delete('/', function (req, res) {
+//   res.send('DELETE request to homepage')
+// })
+
+// app.all('/secret', function (req, res, next) {
+//   console.log('Accessing the secret section ...')
+//   next() // pass control to the next handler
+// })
+
+app.listen(4242)
--- my-express.js
+++ my-express.js
@@ -1,64 +1,118 @@
 
 const http = require('http')
 const url = require('url');
+const fs = require('fs');
+const LOCAL_DATABASE = 'students.json'
 
-class myExpress{
 
-    constructor(){
-      
-      this.init()
-      this.app = this.init()
+class myExpress {
 
-    }
-   
-   init(){
- 
-    const server  =  http.createServer()
-          return server
+    constructor() {
+
+        this.app = this.init()
 
     }
 
+    init() {
 
-    get(path,fn){
-        this.app.on('request', (req,res)=>{
-            fn(req,res)
-            res.end()
-           
+        const server = http.createServer()
+        return server
+
+    }
+
+    getPathname(req) {
+        const request_url = req.url;
+        const { pathname } = url.parse(request_url, true)
+        return pathname
+    }
+    getQuery(req) {
+        const request_url = req.url;
+        const { query } = url.parse(request_url, true)
+        return query
+    }
+
+    get(path, callback) {
+        this.app.on('request', (req, res) => {
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            let pathname = this.getPathname(req)
+            let query = this.getQuery(req)
+
+            // if (query != null) {
+            //     name = query.name
+            // }
+            if (path == pathname && req.method == 'GET') {
+                callback(req, res)
+                res.end()
+            }
         })
 
-    }   
+    }
+// verifier quand on supprime un user et qu'on ajoute un nouveau user l'id soit unique
+    post(path, callback, ) {
+        this.app.on('request', (req, res) => {
 
-    post(){
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            let pathname = this.getPathname(req)
+            let body = ''
 
-    }   
+            if (path == pathname && req.method == 'POST') {
+                req.on('data', chunk => {
+                    body += chunk.toString()
+                })
 
-    put(){
+                req.on('end', () => {
+                    const user = JSON.parse(body)
+                    let data = []
+
+                    if (!fs.existsSync(LOCAL_DATABASE)) {
+                        user.id = 1
+                        data = [user]
+                    } else {
+                        const json = require(`./${LOCAL_DATABASE}`) 
+                        user.id = json.length + 1
+                        json.push(user)
+                        data = json
+                    }
+
+                    fs.writeFileSync(LOCAL_DATABASE, JSON.stringify(data, null, 4))
+                    console.log("L'utilisateur avec l'id " + user.id + " a bien été crée")
+                    callback(req, res)
+                    res.end()
+
+                })
+             
+            }
+        })
+    }
+
+    put() {
 
     }
 
-    delete(){
+    delete() {
 
     }
 
-    all(){
+    all() {
 
     }
-    listen(port){
+    listen(port) {
+    // Recuperer le port en ligne de commande
+        if (typeof port == 'number') {
+            this.app.listen(port)
+        } else {
+            console.log("le port n'est pas un nombre")
+        }
 
-     if(typeof port == 'number'){
-        this.app.listen(port)
-     }else{
-         console.log("le port n'est pas un nombre")
-     }
-      
     }
 
-    render(){
+    render() {
 
     }
 }
 
-function express(){
+function express() {
     return new myExpress()
 }
 
